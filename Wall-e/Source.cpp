4572736ed2d95af1db7bf6acaa12d44d0c4f2b6d
Rafal Skworzec <skworzecrafa@gmail.com>
2014-05-13 13:15:42 +0200

    a

--- Wall-e/Source.cpp
+++ Wall-e/Source.cpp
@@ -91,8 +91,8 @@ void Display()
 
 void CALLBACK Projekcja(HWND hWnd, UINT nMsg, UINT nIDEvent, DWORD dwTime)
 {
-	ret = Robo_AI::Dodge(0, 1023, 0, path);
-	std::cout << "left" << ret[0] << "right" << ret[1] << std::endl;
+	//ret = Robo_AI::Dodge(0, 1023, 0, path);
+	//std::cout << "left" << ret[0] << "right" << ret[1] << std::endl;
 	if (krokL >= 0)
 	{
 		obrotL += krokL;
@@ -358,13 +358,13 @@ void SpecialKeys(int key, int x, int y)
 int main(int argc, char * argv[])
 {
 	////////////////////
-	mclInitializeApplication(NULL, 0);
-	RobotSI1Initialize();
+	//mclInitializeApplication(NULL, 0);
+	//RobotSI1Initialize();
 	/////////////////////////////  
 
 	
-	ret = Robo_AI::Dodge(0, 1023, 0, path);
-	std::cout << "left" << ret[0] << "right" << ret[1] << std::endl;
+	//ret = Robo_AI::Dodge(0, 1023, 0, path);
+	//std::cout << "left" << ret[0] << "right" << ret[1] << std::endl;
 	// inicjalizacja biblioteki GLUT
 	glutInit(&argc, argv);
 	SetTimer(NULL, 1, 30, &Projekcja);
@@ -394,8 +394,8 @@ int main(int argc, char * argv[])
 	// wprowadzenie programu do obs³ugi pêtli komunikatów
 	glutMainLoop();
 	KillTimer(NULL, 1);
-	RobotSI1Terminate();
-	mclTerminateApplication();
+	//RobotSI1Terminate();
+	//mclTerminateApplication();
 
 	return 0;
 }
